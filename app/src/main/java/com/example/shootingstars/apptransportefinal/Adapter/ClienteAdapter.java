f301207d12ee1be7ff091f9a2fa5316c44c987b3
Pablo-Silva <pablo.vargas@opet.edu.com>
2018-11-28 09:51:30 -0200

    Correções

--- app/src/main/java/com/example/shootingstars/apptransportefinal/Adapter/ClienteAdapter.java
+++ app/src/main/java/com/example/shootingstars/apptransportefinal/Adapter/ClienteAdapter.java
@@ -18,36 +18,5 @@ public class ClienteAdapter {
     private EditText editNomeCliente, editEmail, editSenha, editDataNascimento, editCpf, editTextDeficiencia;
     private ClienteRepository repository;
 
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_novo_cliente);
 
-        editNomeCliente = findViewById(R.id.editNomeCliente);
-        editEmail = findViewById(R.id.editEmail);
-        editSenha = findViewById(R.id.editSenha);
-        editDataNascimento = findViewById(R.id.editDataNascimento);
-        editCpf = findViewById(R.id.editCpf);
-        editTextDeficiencia = findViewById(R.id.editTextDeficiencia);
-
-        repository = new ClienteRepository(getApplicationContext());
-    }
-
-    public void salvarFilme(View view){
-        Cliente cliente = new Cliente();
-        cliente.setNomeCliente(editNomeCliente.getText().toString());
-        cliente.setEmailCliente(editEmail.getText().toString());
-        cliente.setSenhaCliente(editSenha.getText().toString());
-        cliente.setCpfCliente(editCpf.getText().toString());
-        cliente.setDeficiencia(editTextDeficiencia.getText().toString());
-
-
-        repository.insert(cliente);
-        callMainActivity();
-    }
-
-    private void callMainActivity() {
-        Intent mainActivity = new Intent(NovoClienteActivity.this,MainActivity.class);
-        startActivity(mainActivity);
-        finish();
-    }
 }
